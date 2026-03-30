f9aa0b84f8692700f90aeaf77c2c9481b2bfcca4
mwine <mwine81@gmail.com>
2026-03-30 15:24:05 -0400

    add benchmarks.parquet file and comment out database connection functions

--- main.py
+++ main.py
@@ -14,33 +14,33 @@ def read_data() -> pl.LazyFrame:
 
 app = FastAPI()
 
-def create_remote_connection():
-    token = os.getenv("TOKEN_RW")
-    return duckdb.connect(f"md:benchmarks?motherduck_token={token}")
+# def create_remote_connection():
+#     token = os.getenv("TOKEN_RW")
+#     return duckdb.connect(f"md:benchmarks?motherduck_token={token}")
 
-def read_database(database: str, product_regex: str | None = None, ndc: str | None = None, benchmark: list[str] | None = None):
-    query = "SELECT * FROM benchmarks"
-    filters = []
-    params = []
+# def read_database(database: str, product_regex: str | None = None, ndc: str | None = None, benchmark: list[str] | None = None):
+#     query = "SELECT * FROM benchmarks"
+#     filters = []
+#     params = []
 
-    if product_regex is not None:
-        filters.append("regexp_matches(product, ?, 'i')")
-        params.append(product_regex)
+#     if product_regex is not None:
+#         filters.append("regexp_matches(product, ?, 'i')")
+#         params.append(product_regex)
 
-    if ndc is not None:
-        filters.append("ndc = ?")
-        params.append(ndc)
+#     if ndc is not None:
+#         filters.append("ndc = ?")
+#         params.append(ndc)
 
-    if benchmark is not None:
-        placeholders = ", ".join("?" * len(benchmark))
-        filters.append(f"benchmark IN ({placeholders})")
-        params.extend(benchmark)
+#     if benchmark is not None:
+#         placeholders = ", ".join("?" * len(benchmark))
+#         filters.append(f"benchmark IN ({placeholders})")
+#         params.extend(benchmark)
 
-    if filters:
-        query += " WHERE " + " AND ".join(filters)
+#     if filters:
+#         query += " WHERE " + " AND ".join(filters)
 
-    with create_remote_connection() as conn:
-        return conn.execute(query, params).pl()
+#     with create_remote_connection() as conn:
+#         return conn.execute(query, params).pl()
 
 @app.get("/item/")
 def get_item(product: str| None = None, ndc: str| None = None, benchmark: Annotated[list[Literal['al-aac', 'mccpdc','big4','fss','nadac','ia-aac']] | None, Query()] = None):
